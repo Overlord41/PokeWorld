09754ab0d5dde5c2279f0e7f9d19d0c7e42cbcd5
Luis <blackkndd@gmail.com>
2022-04-28 09:56:52 -0500

    Creando rutas Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,70 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
-import { useAppDispatch, useAppSelector } from './app/hooks'
-import { increment, sumMore } from './features/Reducers/pokemonReducer'
-import { getCartItems } from './features/Reducers/postsSlice'
-import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { LandingPage } from './pages/LandingPage'
+import { NotFound } from './pages/NotFound'
 
 const App = () => {
-  const value = useAppSelector((state) => state.counter.value)
-  const dispatch = useAppDispatch()
-
-  const handleClick = (): void => {
-    dispatch(increment())
-  }
-
-  const handleClick5 = (): void => {
-    dispatch(sumMore(5))
-  }
-
-  const updatePosts = (): void => {
-    dispatch(getCartItems())
-  }
-
-  useEffect(() => {
-    updatePosts()
-  }, [])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={handleClick}>
-            count is: {value}
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={handleClick5}>
-            Sum More 5
-          </button>
-        </p>
-        <input type="text" disabled={true} />
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom'
+
+export const LandingPage = () => {
+  return (
+    <div>
+      LandingPage <br />
+      <NavLink to="/home">Ingresar</NavLink>
+    </div>
+  )
+}
